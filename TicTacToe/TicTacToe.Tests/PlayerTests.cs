016b9aee5d2dda0a3f8eb4ebc1132db17160d194
Jarne Van Aerde <jarne.vanaerde@protime.eu>
2021-05-04 16:41:24 +0200

    Setup test project

---
+++ TicTacToe/TicTacToe.Tests/PlayerTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace TicTacToe.Tests
+{
+    [TestFixture]
+    public class PlayerTests
+    {
+        [Test]
+        public void AssignCharacter_Success()
+        {
+            var result = AssignCharacter('X');
+
+            Assert.AreEqual('X', result);
+        }
+
+        public char AssignCharacter(char character)
+        {
+            return character;
+        }
+    }
+}
