1fd95d2de6141547a7e9a81bef811f335af679d9
Jarne Van Aerde <jarne.vanaerde@protime.eu>
2021-05-11 14:49:57 +0200

    GameStarted_WithTwoPlayers

---
+++ TicTacToe/TicTacToe.Tests/Tests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TicTacToe.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void GameStarted_WithTwoPlayers()
+        {
+            var players = new List<int>();
+
+            players.Add(1);
+            players.Add(2);
+
+            Assert.AreEqual(2, players.Count());
+        }
+    }
+}
