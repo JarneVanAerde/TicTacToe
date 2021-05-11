5baa90d2cb3c796d5ed7b7c0e14ffd499e1d8635
Jarne Van Aerde <jarne.vanaerde@protime.eu>
2021-05-11 16:14:38 +0200

    EmptyBoard_NotAWinningCondition

--- TicTacToe/TicTacToe.Tests/Tests.cs
+++ TicTacToe/TicTacToe.Tests/Tests.cs
@@ -1,7 +1,16 @@
-﻿namespace TicTacToe.Tests
+﻿using NUnit.Framework;
+
+namespace TicTacToe.Tests
 {
+    [TestFixture]
     public class Tests
     {
-      
+        [Test]
+        public void EmptyBoard_NotAWinningCondition()
+        {
+            bool emptyBoard = true;
+
+            Assert.IsTrue(emptyBoard);
+        }
     }
 }
